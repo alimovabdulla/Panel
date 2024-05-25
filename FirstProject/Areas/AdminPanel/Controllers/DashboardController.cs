ef446338c2bd93d64a6eaca3bcd694e6b54d2431
alimovabdulla <abdullaalimov555@gmail.com>
2024-05-25 15:30:19 +0400

    Add project files.

---
+++ FirstProject/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        
+
+    }
+}
